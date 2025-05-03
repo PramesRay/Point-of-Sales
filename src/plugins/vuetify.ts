6de566dcf38ad4313bcb50b3447b6e7150c1dd87
PramesRay <prames21001@mail.unpad.ac.id>
2025-05-04 00:37:48 +0700

    Change icon config to use mdi icon

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,5 +1,6 @@
 import { createVuetify } from 'vuetify';
-import { aliases, mdi } from 'vuetify/iconsets/mdi-svg';
+import { aliases, mdi } from 'vuetify/iconsets/mdi' // ✅ font-based mdi
+import '@mdi/font/css/materialdesignicons.css'       // ✅ include mdi font CSS
 import { icons } from './mdi-icon'; // Import icons from separate file
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
